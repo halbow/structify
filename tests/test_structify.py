70b4958aa3a0939a7d17e47858f530033167a8cf
Alexis Huberlant <alexis.huberlant@mbrella.eu>
2024-04-14 19:40:43 +0200

    Fix linters

--- tests/test_structify.py
+++ tests/test_structify.py
@@ -20,30 +20,28 @@ def test_structify() -> None:
 
     p = Point(1, 2)
 
-    assert p.add() == 3
+    assert p.add() == 3  # type: ignore
 
 
 def test_structify_args() -> None:
     @struct
     class Point:
-        x: float
-        y: float
+        x: int
+        y: int
 
     @impl
-    def a(
-        self: Point, *args
-    ) -> float:
+    def a(self: Point, *args: int) -> int:
         return self.x + self.y + sum(args)
+
     @impl
-    def b(
-        self: Point, *args, **kwargs
-    ) -> float:
+    def b(self: Point, *args: int, **kwargs: int) -> int:
         return self.x + self.y + sum(args) + sum(kwargs.values())
 
     p = Point(1, 2)
 
-    assert p.a(3, 4) == 10
-    assert p.b(3, 4, x=5, y=6) == 21
+    assert p.a(3, 4) == 10  # type: ignore
+    assert p.b(3, 4, x=5, y=6) == 21  # type: ignore
+
 
 @pytest.mark.parametrize("func", [add_1, add_2])
 def test_structify_impl_with_no_self(func: Callable[..., Any]) -> None:
@@ -60,4 +58,4 @@ def test_structify_raise_attribute_error() -> None:
     p = Point(1, 2)
 
     with pytest.raises(AttributeError, match="'Point' object has no attribute 'missing_attribute'"):
-        p.missing_attribute # type: ignore[attr-defined]
+        p.missing_attribute  # type: ignore[attr-defined]
